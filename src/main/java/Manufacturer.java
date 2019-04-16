11c4f7259199bf2f210bb4c219349afba9c52b85
Daniel Torzala <danieltorzala@gmail.com>
2019-04-15 17:51:18 -0700

    Add constructor and addFactory methods to Manufacturer class

--- src/main/java/Manufacturer.java
+++ src/main/java/Manufacturer.java
@@ -6,7 +6,19 @@ public class Manufacturer {
     private String name;
     private ArrayList<Factory> factories;
     
-    
+    public Manufacturer(String pname, Factory pfactory) {
+        this.name = pname;
+        this.addFactory(pfactory);
+    }
+
+    private boolean addFactory(Factory pfactory) {
+        int arrayLength = factories.size();
+        this.factories.add(pfactory);
+        if(arrayLength + 1 == factories.size())
+            return true;
+        else
+            return false;
+    }
 
     public ArrayList<Factory> getFactories() {
         return factories;
