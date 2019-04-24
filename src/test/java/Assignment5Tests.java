a717a66886e9a468f67abfee3f26b95cb21c516b
Daniel Torzala <danieltorzala@gmail.com>
2019-04-23 17:52:03 -0700

    Fixed implementation of Junit tests

--- src/test/java/Assignment5Tests.java
+++ src/test/java/Assignment5Tests.java
@@ -1,17 +1,17 @@
-/*package test.java;
+package test.java;
 
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import decoratorAndFactoryPattern.java.Factory;
-import decoratorAndFactoryPattern.java.FactoryDecorator;
-import decoratorAndFactoryPattern.java.FactoryImpl;
-import decoratorAndFactoryPattern.java.HybridSpeedupDecorator;
-import decoratorAndFactoryPattern.java.SportsCarSpeedupDecorator;
-import decoratorAndFactoryPattern.java.TruckSpeedupDecorator;
-import mediatorPattern.Manufacturer;
+import decoratorandfactorypattern.Factory;
+import decoratorandfactorypattern.FactoryDecorator;
+import decoratorandfactorypattern.FactoryImpl;
+import decoratorandfactorypattern.HybridSpeedupDecorator;
+import decoratorandfactorypattern.SportsCarSpeedupDecorator;
+import decoratorandfactorypattern.TruckSpeedupDecorator;
+import mediatorpattern.Manufacturer;
 
 public class Assignment5Tests {
     Factory thisFactory;
@@ -23,8 +23,8 @@ public class Assignment5Tests {
     //This tests the methods setName and getName under Manufacturer class
     @Test
     public void testManufacturerSetGetName() {
-        Factory f = new FactoryImpl("a");
-        Manufacturer m = new Manufacturer("Volkswagen",f);
+        Factory f = new FactoryImpl("a", 10.0);
+        Manufacturer m = new Manufacturer("Volkswagen",f, 10.0, 10.0);
         m.setName("Bob");
         assert (m.getName() == "Bob");
     }
@@ -32,9 +32,9 @@ public class Assignment5Tests {
     //This tests the method addFactory under Manufacturer class
     @Test
     public void testManufacturerAddFactory() {
-        Factory f1 = new FactoryImpl("a");
-        Factory f2 = new FactoryImpl("b");
-        Manufacturer m = new Manufacturer("Volkswagen");
+        Factory f1 = new FactoryImpl("a", 10.0);
+        Factory f2 = new FactoryImpl("b", 10.0);
+        Manufacturer m = new Manufacturer("Volkswagen",f1, 10.0, 10.0);
         assert (m.addFactory(f1));
         assert (m.addFactory(f2));
     }
@@ -42,10 +42,10 @@ public class Assignment5Tests {
     //This tests the methods setFactories and getFactories under Manufacturer class
     @Test
     public void testManufacturerSetGetFactories() {
-        Factory f = new FactoryImpl("b");
+        Factory f = new FactoryImpl("a", 10.0);
         ArrayList<Factory> list = new ArrayList<Factory>();
         list.add(f);
-        Manufacturer m = new Manufacturer("Volkswagen",f);
+        Manufacturer m = new Manufacturer("Volkswagen",f, 10.0, 10.0);
         m.setFactories(list);
         assert (m.getFactories() == list);
     }
@@ -53,22 +53,22 @@ public class Assignment5Tests {
     //This tests the methods FactoryImpl.numCarsMade method
     @Test
     public void testFactoryImplNumCarsMade() {
-        Factory f = new FactoryImpl("a");
+        Factory f = new FactoryImpl("a", 10.0);
         assert (f.numCarsMade() == 10);
     }
     
     //This tests the methods FactoryImpl.costPerCar method
     @Test
     public void testFactoryImplCostPerCar() {
-        Factory f = new FactoryImpl("a");
-        assert (f.costPerCar() == 5.0);
+        Factory f = new FactoryImpl("a", 10.0);
+        assert (f.costPerCar() == 1.0);
     }
     
     //This tests the methods set and getManufacturer in FactoryImpl class
     @Test
     public void testFactoryImplSetGetManufacturer() {
-        FactoryImpl f = new FactoryImpl("a");
-        Manufacturer m = new Manufacturer("BMW");
+        FactoryImpl f = new FactoryImpl("a", 10.0);
+        Manufacturer m = new Manufacturer("Volkswagen",f, 10.0, 10.0);
         f.setManufacturer(m);
         assert (f.getManufacturer() == m);
     }
@@ -76,27 +76,27 @@ public class Assignment5Tests {
     //This tests the methods of TruckSpeedupDecorator
     @Test
     public void testTruckSpeedupDecorator() {
-        Factory f = new FactoryImpl("a");
+        Factory f = new FactoryImpl("a", 10.0);
         FactoryDecorator fd = new FactoryDecorator(new TruckSpeedupDecorator(f));
-        assert (fd.costPerCar() == 5.0);
-        assert (fd.numCarsMade() == 17);
+        assert (fd.costPerCar() == 1.0);
+        assert (fd.numCarsMade() == 18);
     }
     
     //This tests the methods of SportsCarSpeedupDecorator
     @Test
     public void testSportsCarSpeedupDecorator() {
-        Factory f = new FactoryImpl("a");
+        Factory f = new FactoryImpl("a", 10.0);
         FactoryDecorator fd = new FactoryDecorator(new SportsCarSpeedupDecorator(f));
-        assert (fd.costPerCar() == 5.0);
+        assert (fd.costPerCar() == 1.0);
         assert (fd.numCarsMade() == 15);
     }
     
     //This tests the methods of HybridSpeedupDecorator
     @Test
     public void testHybridSpeedupDecorator() {
-        Factory f = new FactoryImpl("a");
+        Factory f = new FactoryImpl("a", 10.0);
         FactoryDecorator fd = new FactoryDecorator(new HybridSpeedupDecorator(f));
-        assert (fd.costPerCar() == 5.0);
+        assert (fd.costPerCar() == 1.0);
         assert (fd.numCarsMade() == 18);
     }
-}*/
+}
